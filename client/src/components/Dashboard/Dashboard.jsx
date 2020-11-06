ab237300c68bb9e904d8c12efde00e4aab2e1b29
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2020-11-05 22:17:54 -0500

    working on scroll

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,47 +1,105 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import axios from 'axios';
+import axios from "axios";
 
+import drinkIcon from "../../media/icon-drink.svg";
+import pastaIcon from "../../media/icon-pasta.svg";
+import pizzaIcon from "../../media/icon-pizza.svg";
+import sidesIcon from "../../media/icon-sides.svg";
+import saladIcon from "../../media/icon-salad.svg";
+import chickenIcon from "../../media/icon-chicken.svg";
 
+import "./Dashboard.scss";
 
-import './Dashboard.scss' 
+const Dashboard = ({ location: { location } }) => {
+  const [menu, setMenu] = useState();
+  const [selected, setSelected] = useState('Pizza');
 
-const Dashboard = ({location: { location }}) => {
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get("/api/menu");
 
-    const [menu, setMenu] = useState();
+      setMenu(res.data);
+    }
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        async function fetchData() {
-        const res = await axios.get('/api/menu');
+  const { option, addressType, streetAddress, suiteApt, zipCode } = location;
 
-        setMenu(res.data);
-        }
-        fetchData()
-    }, [])
+  const menuSelectors = [
+    {
+      icon: pizzaIcon,
+      text: "Pizza",
+    },
+    {
+      icon: chickenIcon,
+      text: "Wings",
+    },
+    {
+      icon: sidesIcon,
+      text: "Sides",
+    },
+    {
+      icon: saladIcon,
+      text: "Salad",
+    },
+    {
+      icon: pastaIcon,
+      text: "Pasta",
+    },
+    {
+      icon: drinkIcon,
+      text: "Beverages",
+    },
+  ];
 
-
-    const { option, addressType, streetAddress, suiteApt, zipCode } = location
-    
-    
-
-    return <div className='dashboard-container'>
-        <div className='order-option-address-container'>
-            <div className='inner'>
-                <h2>{option === "Delivery" ? "Food Delivery" : "Carry Out"}</h2>
-                <h3>{option === "Delivery" ? `${streetAddress} ${suiteApt}, ${zipCode}` : "Carry Out"}</h3>
-                
-            </div>
-        </div>
+  const header = (
+    <div className="order-option-address-container">
+      <div className="inner">
+        <h2>{option === "Delivery" ? "Food Delivery" : "Carry Out"}</h2>
+        <h3>
+          {option === "Delivery"
+            ? `${streetAddress}${suiteApt ? " " + suiteApt : ""}, ${zipCode}`
+            : "Carry Out"}
+        </h3>
+      </div>
     </div>
-}
+  );
+
+  const menuSelection = (
+    <div className="menu-selection-container">
+      {menuSelectors.map((item) => {
+        return (
+          <div
+            className={`menu-selector-container ${
+              selected === item.text && "selected-option"
+            }`}
+            key={item.text}
+            onClick={() => setSelected(item.text)}
+          >
+            <img src={item.icon} alt="icon img" />
+            <h3>{item.text}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+
+  return (
+    <div className="dashboard-container">
+      {header}
+      {menuSelection}
+    </div>
+  );
+};
 
 Dashboard.propTypes = {
-    location: PropTypes.object.isRequired,
-}
+  location: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = (state) => ({
-    location: state.location
-  });
+  location: state.location,
+});
 
-export default connect(mapStateToProps, null)(Dashboard)
+export default connect(mapStateToProps, null)(Dashboard);
